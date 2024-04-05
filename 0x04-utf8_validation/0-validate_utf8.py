2f76557fb2fd800738d03eb2e1cea2da1248985d
GArdennes <thekevin.afachao@gmail.com>
2024-04-05 02:24:09 -0700

    Task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,27 +14,19 @@ def validUTF8(data):
     Returns:
       True if data is valid UTF-8 encoding, else False.
     """
-    count_ones = 0
+    count_bytes = 0
     for num in data:
-        # Extract the 8 least significant bits
-        num &= 0xff
-        if count_ones == 0:
-            # First byte of a character
-            if num >= 0xc0:
-                if num >= 0xf0:
-                    count_ones = 4
-                elif num >= 0xe0:
-                    count_ones = 3
-                elif num >= 0xc2:
-                    count_ones = 2
-                else:
-                    return False
-            else:
-                # Valid single-byte character
-                count_ones = 0
-        else:
-            # Subsequent bytes of a character
-            if num < 0x80 or num >= 0xc0:
+        if count_bytes == 0:
+            if (num >> 5) == 0b110:
+                count_bytes = 1
+            elif (num >> 4) == 0b1110:
+                count_bytes = 2
+            elif (num >> 3) == 0b11110:
+                count_bytes = 3
+            elif (num >> 7) != 0:
                 return False
-            count_ones -= 1
-    return count_ones == 0
+        else:
+            if (num >> 6) != 0b10:
+                return False
+            count_bytes -= 1
+    return count_bytes == 0
