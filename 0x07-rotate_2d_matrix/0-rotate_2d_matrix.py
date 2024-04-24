77ffa7a3d0e1857546f361992fd58bdf4b3bf8cb
GArdennes <thekevin.afachao@gmail.com>
2024-04-24 00:45:26 -0700

    Task

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,12 +8,6 @@ def rotate_2d_matrix(matrix):
     """
     Given a square matrix rotate it 90 degrees clockwise
     """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
     for i in range(len(matrix)):
         matrix.reverse()
     for i in range(len(matrix)):
